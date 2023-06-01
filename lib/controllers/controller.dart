e44667158aa80f5ebcf1020b2dd31fc5cf8fee97
Juan Luis Ávila Montoya <jlam@pop-os.localdomain>
2023-05-31 21:37:54 -0500

    Template email

--- lib/controllers/controller.dart
+++ lib/controllers/controller.dart
@@ -1,14 +1,18 @@
 import 'package:get/get.dart';
 import 'package:sendgrid_mailer/sendgrid_mailer.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class mainController extends GetxController{
   
   void sendMail() async {
   final mailer = Mailer('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');
   final toAddress = Address('juanluis0217@gmail.com');
-  final fromAddress = Address('juanl.avila@urosario.edu.co');
-  final content = Content('text/plain', 'Su reserva ha sido confirmada exitosamente.');
-  final subject = 'Reserva de espacio confirmada';
+  const fromAddress = Address('juanl.avila@urosario.edu.co');
+
+  final htmlTemplate = await rootBundle.loadString('assets/email_templates/reservation_accepted.html');
+
+  final content = Content('text/html', htmlTemplate);
+  const subject = 'Reserva de espacio confirmada';
   final personalization = Personalization([toAddress]);
 
   final email =
